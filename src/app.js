8a993129e8c747d114ad2097b354899ecc1cceae
Abel <abelmamani186f.com>
2025-07-22 17:19:26 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
+import cors from "cors";
 import productRoute from "./routes/product.route.js";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/products', productRoute);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
